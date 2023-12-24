25da873dbd119ad617f0a62f36190ee830b55b92
nhatnkse16 <nguyenkimnhat0110@gmail.com>
2023-12-24 18:16:10 +0700

    24/12

--- src/server.js
+++ src/server.js
@@ -1,3 +1,10 @@
+/** 
+ * 
+ * CẤU HÌNH EXPRESS 
+ * 
+ */
+
+
 /** Khai báo dotevn */
 require('dotenv').config();
 
@@ -16,14 +23,41 @@ app.set('view engine', 'ejs');
 
 
 /** Khai báo server */
-const port = process.env.PORT || 8888;              //port
+const port = process.env.PORT || 8888;                  //port
 const hostname = process.env.HOST_NAME;
-app.listen(port, hostname, () => {
-    console.log(`App listening on port ${port}`)
-});
+// app.listen(port, hostname, () => {
+//     console.log(`App listening on port ${port}`)
+// });
 
+const server = require('http').Server(app);
+server.listen(port);
 
 /** Tạo hàm getter cơ bản */
 app.get('/', function (req, res) {
     res.render('index');
 });
+
+
+
+/** 
+ * 
+ * CẤU HÌNH SOCKET IO
+ * 
+ */
+
+
+const io = require("socket.io")(server);
+
+
+/** Hàm lắng nghe sự kiện kết nối */
+io.on('connection', function (socket) {                   // Lắng nghe có người kết nối => on
+    console.log("Có người kết nối --> " + socket.id);
+
+    /** Hàm Lắng nghe sự kiện ngắt kết nối */
+    socket.on('disconnect', function () {
+        console.log("Đã ngắt kết nối  --> " + socket.id);
+        console.log('\n*****************************');
+    })
+
+});
+
